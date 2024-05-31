c742ebfa53346ccf3914775954090f7ec87dcc02
Davor <davor.hoic@gmail.com>
2024-05-31 17:25:37 +0200

    Creating Layout

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+    <H1>This is contact page</H1>
+    <ul>
+        <li>First Name: Davor</li>
+        <li>Last Name: Hoić</li>
+    </ul>
+</x-layout>
